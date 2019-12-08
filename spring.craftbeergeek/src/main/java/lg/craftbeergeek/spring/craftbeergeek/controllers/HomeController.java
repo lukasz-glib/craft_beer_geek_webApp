86e2a9232a96eb94cb91da6b57b3315d5e93c348
Lukasz Glib <lukasz.gl28@gmail.com>
2019-12-08 22:42:37 +0100

    add HomeController v_1

---
+++ spring.craftbeergeek/src/main/java/lg/craftbeergeek/spring/craftbeergeek/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package lg.craftbeergeek.spring.craftbeergeek.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+
+    @GetMapping
+    public String mainView(){
+        return "home";
+    }
+}
