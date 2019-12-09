afdeb0c16d2f57396e24e7f397320f60c34985a3
Lukasz Glib <lukasz.gl28@gmail.com>
2019-12-09 18:39:01 +0100

    add config

---
+++ spring.craftbeergeek/src/main/java/lg/craftbeergeek/spring/craftbeergeek/dtos/RegistrationDataDTO.java
@@ -0,0 +1,20 @@
+package lg.craftbeergeek.spring.craftbeergeek.dtos;
+
+import lombok.Data;
+
+import javax.validation.constraints.*;
+
+@Data
+public class RegistrationDataDTO {
+
+    @NotBlank @Size(min = 3, max = 12)
+    private String username;
+    @NotBlank @Email
+    private String email;
+    @NotBlank @Size(min = 4, max = 12)
+    private String password;
+    @NotBlank @Size(min = 4, max = 12)
+    private String repassword;
+    @NotNull @AssertTrue
+    private Boolean termsAcceptance;
+}
