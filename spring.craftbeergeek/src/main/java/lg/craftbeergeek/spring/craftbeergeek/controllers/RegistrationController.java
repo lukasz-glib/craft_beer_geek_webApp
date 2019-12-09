975048189757665e48cb753811b284bb22791fb1
Lukasz Glib <lukasz.gl28@gmail.com>
2019-12-09 18:42:12 +0100

    add RegistrationController

---
+++ spring.craftbeergeek/src/main/java/lg/craftbeergeek/spring/craftbeergeek/controllers/RegistrationController.java
@@ -0,0 +1,45 @@
+package lg.craftbeergeek.spring.craftbeergeek.controllers;
+
+
+import lg.craftbeergeek.spring.craftbeergeek.dtos.RegistrationDataDTO;
+import lg.craftbeergeek.spring.craftbeergeek.services.RegistrationService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+
+
+    private final RegistrationService registrationService;
+
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @GetMapping
+    public String getRegistrationPage (Model model) {
+        model.addAttribute("registrationData", new RegistrationDataDTO());
+        return "register/form";
+    }
+
+    @PostMapping
+    public String processRegistrationPage(@ModelAttribute("registrationData")
+                                              @Valid RegistrationDataDTO registrationData, BindingResult result){
+        if (result.hasErrors()) {
+            return "register/form";
+        }
+        registrationService.register(registrationData);
+        return "redirect:/";
+    }
+
+
+}
+
