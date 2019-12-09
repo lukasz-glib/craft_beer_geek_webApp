fa1d269f8486f83e07084f04d5102442d0b298c3
Lukasz Glib <lukasz.gl28@gmail.com>
2019-12-09 18:43:12 +0100

    add RegistrationService

---
+++ spring.craftbeergeek/src/main/java/lg/craftbeergeek/spring/craftbeergeek/services/RegistrationService.java
@@ -0,0 +1,9 @@
+package lg.craftbeergeek.spring.craftbeergeek.services;
+
+import lg.craftbeergeek.spring.craftbeergeek.dtos.RegistrationDataDTO;
+
+public interface RegistrationService {
+
+    void register(RegistrationDataDTO registrationData);
+
+}
---
+++ spring.craftbeergeek/src/main/java/lg/craftbeergeek/spring/craftbeergeek/services/impl/DefaultRegistrationService.java
@@ -0,0 +1,43 @@
+package lg.craftbeergeek.spring.craftbeergeek.services.impl;
+
+
+import lg.craftbeergeek.spring.craftbeergeek.domain.entities.Role;
+import lg.craftbeergeek.spring.craftbeergeek.domain.entities.User;
+import lg.craftbeergeek.spring.craftbeergeek.domain.repositories.RoleRepository;
+import lg.craftbeergeek.spring.craftbeergeek.domain.repositories.UserRepository;
+import lg.craftbeergeek.spring.craftbeergeek.dtos.RegistrationDataDTO;
+import lg.craftbeergeek.spring.craftbeergeek.services.RegistrationService;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class DefaultRegistrationService implements RegistrationService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public DefaultRegistrationService(PasswordEncoder passwordEncoder, UserRepository userRepository, RoleRepository roleRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void register(RegistrationDataDTO registrationData) {
+        ModelMapper mapper = new ModelMapper();
+        User user = mapper.map(registrationData, User.class); //przypisujemy wartości
+        user.setActive(Boolean.TRUE);
+        String encodedPassword = passwordEncoder.encode(registrationData.getPassword()); //szyfrowanie hasła
+        user.setPassword(encodedPassword);
+        Role roleUser = roleRepository.getByName("ROLE_USER");
+        user.getRoles().add(roleUser);
+        userRepository.save(user);
+
+
+    }
+}
